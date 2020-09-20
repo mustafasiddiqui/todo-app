ea1ecbce9ac64890856300b6bac84950a1f44bf5
Mustafa Siddiqui <mustafasiddiqui79@gmail.com>
2020-09-20 08:28:50 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Todo from './Todo'
 import './App.css';
 
+let name = "Mustafa";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('');
+  }
+
+  // console.log(todos);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>To-do app made by {name}</h1>
+      <form>
+        <input type="text" value={input}
+          onChange={e => setInput(e.target.value)} />
+        <button disabled={!input} type="submit" onClick={handleSubmit}>Add Todo</button>
+      </form>
+
+      {todos.map((todo, i) => (
+        <Todo title={todo} key={i} />
+      ))}
     </div>
   );
 }
---
+++ src/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Todo.css'
+
+const Todo = (props) =>{
+    return(<div className="todo">
+    <h2>{props.title}</h2>
+    {/* <p>{props.description}</p> */}
+  </div>);
+}
+
+export default Todo;
